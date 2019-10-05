03f386d90f8e62839793eb473d8a06f76a22edf9
Andrei Sukonin <andrei_sukonin@epam.com>
2019-10-05 14:20:11 +0300

    added spring mvc dependency added spring mvc config added controller

--- src/main/java/com/epam/crud/config/MainWebAppInitializer.java
+++ src/main/java/com/epam/crud/config/MainWebAppInitializer.java
@@ -1,17 +1,14 @@
 package com.epam.crud.config;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-public class MainWebAppInitializer extends
-    WebMvcConfigurationSupport implements WebApplicationInitializer {
+public class MainWebAppInitializer implements WebApplicationInitializer {
 
   @Override
   public void onStartup(ServletContext servletContext) {
