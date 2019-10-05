91bec1a8ff8b44c9d8c8fa36c0c56b4039efa9cc
Andrei Sukonin <andrei_sukonin@epam.com>
2019-10-05 15:03:00 +0300

    added h2 database added datasource config added jdbctemplate bean added data.sql and schema.sql scripts

---
+++ src/main/java/com/epam/crud/config/DatabaseConfig.java
@@ -0,0 +1,28 @@
+package com.epam.crud.config;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@ComponentScan("com.epam.crud.repository")
+public class DatabaseConfig {
+
+  @Bean
+  public DataSource dataSource() {
+    return new EmbeddedDatabaseBuilder()
+        .setType(EmbeddedDatabaseType.H2)
+        .addScript("classpath:schema.sql")
+        .addScript("classpath:data.sql")
+        .build();
+  }
+
+  @Bean
+  public JdbcTemplate getJdbcTemplate() {
+    return new JdbcTemplate(dataSource());
+  }
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE User
+(
+  id       int,
+  username varchar(255),
+  password varchar(255),
+);
