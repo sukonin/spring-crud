192ec95003dedab7852179e9de89791f83295cac
Andrei Sukonin <andrei_sukonin@epam.com>
2019-10-08 13:14:30 +0300

    -

--- src/main/java/com/epam/crud/config/WebConfig.java
+++ src/main/java/com/epam/crud/config/WebConfig.java
@@ -8,6 +8,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
-@ComponentScan(basePackageClasses = ApplicationConfig.class)
 public class WebConfig implements WebMvcConfigurer {
 }
